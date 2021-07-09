15926c4b6052acd2b36194c5dfc0e73617e9b280
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-07-09 15:14:06 +0200

    paso 8

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
 import Tabla from './Tabla';
-
+import Form from './Form';
 
 class App extends Component {
     state = {
-        personas: [
-            {
-                nombre: 'Clark',
-                apellido: 'Kent',
-            },
-            {
-                nombre: 'Bruce',
-                apellido: 'Wayne',
-            },
-            {
-                nombre: 'Peter',
-                apellido: 'Parker',
-            },
-            {
-                nombre: 'Selina',
-                apellido: 'Kyle',
-            },
-        ],
+        personas: [],
     }
 
     eliminarPersona = (indice) => {
@@ -33,14 +16,20 @@ class App extends Component {
             }),
         });
     }
+    enviarFormulario = (persona) => {
+        this.setState({ personas: [...this.state.personas, persona] });
+    }
+
     render(){
         const {personas} = this.state;
         return (
             <div className="container">
                 <h1>Tutorial de React de Neoguias!</h1>
                 <Tabla datosPersonas={personas} eliminarPersona={this.eliminarPersona} />
+                <Form enviarFormulario={this.enviarFormulario} />
             </div>
-        )
+        );
     }
+
 }
 export default App;
